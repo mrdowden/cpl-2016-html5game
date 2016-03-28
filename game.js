abbd23a1a6b1adbf3c9cd6edda9b6a4c169ab3dd
Michael Dowden <michael@dowden.us>
2016-03-28 14:53:36 +0000

    Added bullets

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ var colors = [ "#ff0000", "#ee0000", "#dd0000", "#cc0000", "#bb0000", "#aa0000",
 var color = 0;
 
 var keys = {
+    32: "space",
     37: "left",
     38: "up",
     39: "right",
@@ -23,12 +24,49 @@ window.addEventListener("keyup", function(e) {
    pressed[keys[e.keyCode]] = false; 
 });
 
-var ship = new Image();
-ship.src = "images/ship-f3.png";
-
 var player = { x: 50, y: 50, speed: 0.5, friction: 0.9 };
 
+var animations = {
+    ship: {
+        image: new Image(),
+        frame: 0,
+        frames: 3,
+        frameWidth: 204 / 3,
+        time: 0,
+        speed: 200
+    },
+    bullet: {
+        image: new Image(),
+        frame: 0,
+        frames: 5,
+        frameWidth: 55 / 5,
+        time: 0,
+        speed: 100
+    }
+};
+animations.ship.image.src = "images/ship-f3.png";
+animations.bullet.image.src = "images/bullet-f5.png";
 
+var advanceAnimations = function(elapsed) {
+    Object.keys(animations).forEach(function(name) {
+        var anim = animations[name];
+        anim.time += elapsed;
+        while(anim.time > anim.speed) {
+            anim.time -= anim.speed;
+            anim.frame++;
+            if(anim.frame >= anim.frames) {
+                anim.frame = 0;
+            }
+        }
+        anim.x = anim.frame * anim.frameWidth;
+    });
+}
+var drawAnimation = function(context, name, x, y) {
+    var anim = animations[name];
+    context.drawImage(anim.image, anim.x, 0, anim.frameWidth, anim.image.height, x, y, anim.frameWidth, anim.image.height);
+}
+
+var bullets = [];
 var lastFrameTime = null;
 
 var render = function(time) {
@@ -38,8 +76,9 @@ var render = function(time) {
     var elapsed = time - lastFrameTime;
     lastFrameTime = time;
     
-    xspeed = 0;
-    yspeed = 0;
+    advanceAnimations(elapsed);
+    
+    var xspeed = 0, yspeed = 0;
     if(pressed["left"]) {
         xspeed = -player.speed;
     }
@@ -52,16 +91,23 @@ var render = function(time) {
     if(pressed["down"]) {
         yspeed = player.speed;
     }
-    xspeed *= player.friction;
-    yspeed *= player.friction;
+    if(pressed["space"]) {
+        bullets.push({
+            x: player.x + (animations.ship.frameWidth / 2) - (animations.bullet.frameWidth / 2),
+            y: player.y - animations.bullet.image.height
+        });
+    }
+    
+    //xspeed *= player.friction;
+    //yspeed *= player.friction;
     player.x += xspeed * elapsed;
     player.y += yspeed * elapsed;
     
     //Constrain X,Y
     if(player.x < 0) player.x = 0;
-    if(player.x + 50 > canvas.width) player.x = canvas.width - 50;
+    if(player.x + animations.ship.frameWidth > canvas.width) player.x = canvas.width - animations.ship.frameWidth;
     if(player.y < 0) player.y = 0;
-    if(player.y + 50 > canvas.height) player.y = canvas.height - 50;
+    if(player.y + animations.ship.image.height > canvas.height) player.y = canvas.height - animations.ship.image.height;
     
 /*    context.fillStyle = colors[color];
     color++;
@@ -69,7 +115,13 @@ var render = function(time) {
         color = 0;
     }*/
     context.clearRect(0, 0, canvas.width, canvas.height);
-    context.drawImage(ship, 0, 0, ship.width, ship.height, player.x, player.y, ship.width, ship.height);
+    drawAnimation(context, "ship", player.x, player.y);
+    for(var i = 0; i < bullets.length; i++) {
+        var bullet = bullets[i];
+        drawAnimation(context, "bullet", bullet.x, bullet.y);
+        bullet.y -= 1 * elapsed;
+    }
+    //context.drawImage(ship, 0, 0, ship.width, ship.height, player.x, player.y, ship.width, ship.height);
     //context.fillRect(player.x, player.y, 50, 50);
     window.requestAnimationFrame(render);
 }
